47a60a91f64ace287ef2208af7561f03edac10fe
Rafael Azevedo <rafael.peixoto.de.azevedo@gmail.com>
2024-05-09 15:05:49 +1000

    update dependencies and add Moshi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -10,10 +11,16 @@ repositories {
 }
 
 val kotestVersion = "5.8.1"
-val arrowVersion  = "1.2.1"
+val arrowVersion  = "1.2.4"
+val moshiVersion  = "1.15.1"
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("com.squareup.moshi:moshi:$moshiVersion")
+    implementation("com.squareup.moshi:moshi-kotlin:$moshiVersion")
+    implementation("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
 
     implementation(platform("io.arrow-kt:arrow-stack:$arrowVersion"))
     implementation("io.arrow-kt:arrow-core")
