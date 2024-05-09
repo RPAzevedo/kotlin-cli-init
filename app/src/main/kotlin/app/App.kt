a33de0eda595c0141448925109d3ed067b82b203
Rafael Azevedo <rafael.peixoto.de.azevedo@gmail.com>
2024-05-09 15:07:29 +1000

    add JSON example

--- app/src/main/kotlin/app/App.kt
+++ app/src/main/kotlin/app/App.kt
@@ -1,9 +1,25 @@
 package app
 
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
+
+val jsonAdapter: Moshi = Moshi.Builder().build()
+@OptIn(ExperimentalStdlibApi::class)
+val greetingAdapter: JsonAdapter<Greeting> = jsonAdapter.adapter<Greeting>()
+
 class App {
-    val greeting = "Hello World!"
+    fun greetFromJson(jsonGreeting: String): String {
+        val greeting = greetingAdapter.fromJson(jsonGreeting)!!
+        return "${greeting.message} ${greeting.person}!"
+    }
 }
 
+@JsonClass(generateAdapter = true)
+data class Greeting(val message: String, val person: String)
+
 fun main() {
-    println(App().greeting)
+    val jsonGreeting = greetingAdapter.toJson(Greeting("Hello", "Raf"))
+    println(App().greetFromJson(jsonGreeting))
 }
--- app/src/test/kotlin/app/AppTest.kt
+++ app/src/test/kotlin/app/AppTest.kt
@@ -1,10 +1,19 @@
 package app
 
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class AppTest : StringSpec({
+    val jsonAdapter: Moshi = Moshi.Builder().build()
+
     "App greets" {
-        App().greeting shouldBe "Hello World!"
+        @OptIn(ExperimentalStdlibApi::class)
+        val greetingAdapter: JsonAdapter<Greeting> = jsonAdapter.adapter<Greeting>()
+        val jsonGreeting = greetingAdapter.toJson(Greeting("Hi", "Gus"))
+
+        App().greetFromJson(jsonGreeting) shouldBe "Hi Gus!"
     }
 })
